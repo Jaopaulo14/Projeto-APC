6ddf4fda520d07dbd42bd86da69e0ef0e5ff8f36
João Paulo Soares <165195769+Jaopaulo14@users.noreply.github.com>
2024-08-24 16:42:41 -0300

    Update Código incompleto.c

--- Código incompleto.c
+++ Código incompleto.c
@@ -29,7 +29,7 @@ int compra(produto carrinho[1001],int indice_carrinho, double saldo){
   Nesse caso vamos comprar o máximo de produtos que der */
 
   //printf("%.2lf\n",(saldo - (item.quantidade * item.preco)));
-  
+
   if((saldo - (item.quantidade * item.preco)) < 0){
 
     /* Vamos verificar o máximo de produtos que podemos retirar até o saldo poder cobrir*/
@@ -37,11 +37,11 @@ int compra(produto carrinho[1001],int indice_carrinho, double saldo){
 
       //printf("O saldo eh %.2lf\n",saldo);
       //printf("diminuindo o preco do produto:%.2lf\n",(saldo - item.preco * (item.quantidade - qtd_prod_removidos)));
-      
+
       if((saldo - item.preco * (item.quantidade - qtd_prod_removidos)) < 0){
-        
+
         qtd_prod_removidos++;
-          
+
        }
     }
   }else{
@@ -81,7 +81,7 @@ int compra(produto carrinho[1001],int indice_carrinho, double saldo){
     }else{
 
       /* se nao houver semelhante vamos adicionar ao carrinho*/
-      
+
       strcpy(carrinho[indice_carrinho].nome,item.nome);
       carrinho[indice_carrinho].preco = item.preco;
       carrinho[indice_carrinho].quantidade = item.quantidade;
@@ -99,7 +99,7 @@ int compra(produto carrinho[1001],int indice_carrinho, double saldo){
 
 void mostra(produto carrinho [1001],int indice_carrinho){
 
-  int i, k, indice_ascendente[1001];
+  int i, k,l;
   produto aux;
   double total;
 
@@ -110,7 +110,12 @@ void mostra(produto carrinho [1001],int indice_carrinho){
 
     for(k = i+1; k < indice_carrinho; k++){
 
-      if(carrinho[i].nome[0] > carrinho[k].nome[0]){
+      for(l = 0; l < strlen(carrinho[i].nome); l++){
+        if(carrinho[i].nome[l] != carrinho[k].nome[l]){
+          break;
+        }
+      }
+      if(carrinho[i].nome[l] > carrinho[k].nome[l]){
 
         /* bota o produto no aux*/
         strcpy(aux.nome,carrinho[i].nome);
@@ -134,7 +139,7 @@ void mostra(produto carrinho [1001],int indice_carrinho){
   for(i = 0; i < indice_carrinho; i++){
 
     printf("%s ",carrinho[i].nome);
-    printf("%d X ",carrinho[i].quantidade);
+    printf("%d x ",carrinho[i].quantidade);
     printf("%.2lf = ",carrinho[i].preco);
     printf("%.2lf\n",carrinho[i].preco * carrinho[i].quantidade);
 
@@ -188,8 +193,8 @@ int remover(produto carrinho[1001],int indice_carrinho,double saldo){
     /* se a quantidade dos produtos depois da remoção for 0, então temos que tirar ele do carrinho 
     para isso vamo mover ele para o último indice e decrementa o indice_carrinho*/
 
-    if((carrinho[i].quantidade - qtd_removidas) == 0){
-      
+    if((carrinho[i].quantidade - qtd_removidas) <= 0){
+
       for(k = i+1 ; k < indice_carrinho; k++, i++){
 
         /*vamos copiar o que está no indice da frente no anterior*/
@@ -222,16 +227,16 @@ double atualizar(produto carrinho[1001], int indice_carrinho, double saldo){
   int i,indice_atualizar,aux,qtd_prod_removidos,k;
   char nome[100], existe_produto;
   double novo_preco,novo_saldo,preco_antigo;
-
+  
+  aux = 1;
+  
   scanf("%s",nome);
   getchar();
   scanf("%lf",&novo_preco);
   getchar();
 
-
   /*vamos verificar se o item está no carrinho comparando os nomes */
   for(i = 0; i < indice_carrinho; i++){
-
     aux = strcmp(nome,carrinho[i].nome);
 
     if(aux == 0){
@@ -279,20 +284,32 @@ double atualizar(produto carrinho[1001], int indice_carrinho, double saldo){
 
     /* Agora que temos a quantidade máxima de produtos que foram removidos, vamos
     atualizar nas informações do produto*/
-    carrinho[i].quantidade = carrinho[i].quantidade - qtd_prod_removidos;
 
+    //printf("a quantidade antes do A eh: %d\n",carrinho[i].quantidade);
+    carrinho[i].quantidade = carrinho[i].quantidade - qtd_prod_removidos;
+    //printf("a quantidade depois do A eh: %d\n",carrinho[i].quantidade);
+    /* se depois da remocao a quantidade for zero entao temos que tiralo do carrinho*/
+    if(carrinho[i].quantidade == 0){
+
+      /* vamos passa ele para o ultimo indice para depois remover*/
+      for(k = i+1 ; k < indice_carrinho; k++, i++){
+
+        /*vamos copiar o que está no indice da frente no anterior*/
+        strcpy(carrinho[i].nome,carrinho[k].nome);
+        carrinho[i].preco = carrinho[k].preco;
+        carrinho[i].quantidade = carrinho[k].quantidade;
+
+      } 
+    }
     /* se quantidade de produtos adicionados der zero, não vamos adicionar nada */
-    
     /*agora vamos atualizar o saldo*/
     saldo = saldo - carrinho[i].preco * carrinho[i].quantidade;
-    
+
     /* vamos retornar o novo saldo depois da troca de preco*/      
     return saldo;
 
   }else{
-    
     printf("ERRO: O produto %s nao esta no carrinho\n",nome);
-
     return saldo;
   }
 
@@ -300,7 +317,7 @@ double atualizar(produto carrinho[1001], int indice_carrinho, double saldo){
 
 int main() {
 
-  int Q,i,indice_carrinho, a,aux;
+  int Q,i,indice_carrinho, a,aux,k,aux_A;
   double dinheiro_disponivel;
   char operacao;
 
@@ -344,6 +361,7 @@ int main() {
     if(operacao == 'R'){
 
       indice_carrinho = remover(carrinho,indice_carrinho,dinheiro_disponivel);
+      //printf("o indice eh %d\n",indice_carrinho);
 
       /* atualizamos o saldo disponivel */
       dinheiro_disponivel = dinheiro_disponivel + carrinho[indice_carrinho + 1].preco * carrinho[indice_carrinho + 1].quantidade;
@@ -351,8 +369,17 @@ int main() {
     }
     if(operacao == 'A'){
 
-    dinheiro_disponivel =  atualizar(carrinho,indice_carrinho,dinheiro_disponivel);
+      dinheiro_disponivel =  atualizar(carrinho,indice_carrinho,dinheiro_disponivel);
 
+      /*para o caso de que tenha que remover o item do carrinho, como ja passamos
+      ele para o ultimo indice so decrementar o indice_carrinho*/
+      //printf("tem %d quantidades\n",carrinho[indice_carrinho].quantidade);
+      for(k = 0; k < indice_carrinho; k++){
+        if(carrinho[k].quantidade == 0){
+          indice_carrinho--;
+          //printf("no A eh %d\n",indice_carrinho);
+        }
+      }
     }
   }
   return 0;
